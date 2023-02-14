b38defaed8f31fff51aa89e20de041458e3c798d
S0lidSnak3 <a235697@uach.mx>
2023-02-14 14:22:44 -0600

    Se modifico la pagina y recordar modificar el driver

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -9,8 +9,9 @@ public class BaseClass {
 	final public String EMAIL = "";
 	final public String PASS = "";
 	
-	private final String _driverUrl = "/Users/jovasguzman/Selenium/drivers/chromedriver_mac64/chromedriver";
-	public final String pageUrl = "https://advantageonlineshopping.com";
+	private final String _driverUrl = "C:\\SDETAcademy\\Drivers\\chromedriver.exe";
+
+	public final String pageUrl = "https://academybugs.com/find-bugs/";
 	public WebDriver driver = new ChromeDriver();
 	
 	@BeforeClass
