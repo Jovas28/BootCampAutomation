dfc3925ca76ea72d150afaec4e38de66df069b93
Samuel Martinez <samuel.arreola@softtek.com>
2023-02-14 16:11:32 -0600

    finishing the details of TC

--- src/test/java/tests/SearchItem.java
+++ src/test/java/tests/SearchItem.java
@@ -10,7 +10,6 @@ public class SearchItem extends BaseClass {
     public void RunTC06() {
         driver.findElement(By.cssSelector("#ec_product_image_effect_dnk-yellow-shoes > .ec_image_link_cover")).click();
         driver.findElement(By.name("ec_search")).click();
-        driver.findElement(By.name("ec_search")).click();
         driver.findElement(By.name("ec_search")).sendKeys("TSHIRT");
         driver.findElement(By.cssSelector("form:nth-child(1) > input:nth-child(3)")).click();
     }
