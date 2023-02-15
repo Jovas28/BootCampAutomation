3ba8f3617264eb69c6cb314ab94af0ae3fbd1786
S0lidSnak3 <a235697@uach.mx>
2023-02-14 18:11:32 -0600

    Terminos y condiciones

---
+++ src/test/java/tests/TermsAndConditions.java
@@ -0,0 +1,13 @@
+package tests;
+import base.BaseClass;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class TermsAndConditions extends BaseClass{
+	
+	@Test
+	public void run() {
+		 driver.findElement(By.linkText("Terms & Conditions")).click();
+	}
+}
