4bff9e6a12e336d84e0851944c934ecf5c629204
S0lidSnak3 <a235697@uach.mx>
2023-02-15 11:51:36 -0600

    TC-09 y cambios en los nombres de los metodos por TC##

--- src/test/java/tests/TestCases1_3.java
+++ src/test/java/tests/TestCases1_3.java
@@ -11,14 +11,14 @@ public class TestCases1_3 extends BaseClass {
 
 	// TC-001 Products displayed on main page
 	@Test
-	public void mainPageProducts() {
+	public void TC01() {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
 	}
 
 	// TC-002 Product clicked should be display detailed information
 	@Test(dependsOnMethods = "mainPageProducts")
-	public void productDetails() {
+	public void TC02() {
 		WebElement profesionalSuit = driver
 				.findElement(By.xpath("//div[@id='ec_product_image_effect_3981370']//a[@class='ec_image_link_cover']"));
 		profesionalSuit.click();
@@ -26,7 +26,7 @@ public class TestCases1_3 extends BaseClass {
 
 	// TC-003 Product details, quantity selected and currency selection
 	@Test(dependsOnMethods = "productDetails")
-	public void productQuantity() {
+	public void TC03() {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,120)");
 		WebElement productQty = driver.findElement(By.xpath("//input[@id='ec_quantity_29']"));
